b98b6ac2a1b23f0f781071320fb2feab11cef485
Spacelama587 <81617658+Spacelama587@users.noreply.github.com>
2023-06-20 17:31:09 +0530

    Add mobile Menu

---
+++ script.js
@@ -0,0 +1,24 @@
+const logo = document.querySelector('.logo');
+const menu_btn = document.querySelector('.ham');
+const mobile_menu = document.querySelector('.mobile-menu');
+const menu_links = mobile_menu.querySelectorAll('a');
+
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+
+  if (logo.classList.contains('hide-logo')) {
+    logo.classList.remove('hide-logo');
+  } else {
+    logo.classList.add('hide-logo');
+  }
+});
+
+// Close mobile menu when a link is clicked
+menu_links.forEach((link) => {
+  link.addEventListener('click', function () {
+    menu_btn.classList.remove('is-active');
+    mobile_menu.classList.remove('is-active');
+    logo.classList.remove('hide-logo');
+  });
+});
